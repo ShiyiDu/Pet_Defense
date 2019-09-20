46a6a5457a76977bac7ed92162654c9e6f35c139
shiyi du <shiyi.du@qq.com>
2019-09-20 13:14:19 -0400

    Project Created

---
+++ Pet/Assets/C#/Cat.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cat : MonoBehaviour, Pet
+{
+    // Start is called before the first frame update
+
+    public int GetHealth()
+    {
+        return 0;
+    }
+
+    public void Kill()
+    {
+
+    }
+
+    void Start()
+    {
+        print("Started");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Pet/Assets/C#/Pet.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface Pet
+{
+    //kill the pet
+    void Kill();
+
+    //return the current health remaining
+    int GetHealth();
+}

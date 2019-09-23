407ace3eb73797f975ef0b0db8127199890d14f8
Leo <shiyi.du@qq.com>
2019-09-23 10:19:21 -0400

    Reguar commit
    making progresses :)

---
+++ Pet/Assets/C#/DoorControl.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.Events;
+
+public class DoorControl : MonoBehaviour
+{
+    public DoorControl targetDoor;
+    public bool onWayDoor = false;
+    public bool woodDoor = false;
+    public bool stairs = false;
+    private bool playerNearby = false;
+    private bool controlEnabled = true;
+    private static GameObject player;
+    //private GameObject currentScene;
+
+    private void OnDrawGizmos()
+    {
+        if (targetDoor) {
+            if (!targetDoor.targetDoor && !onWayDoor) targetDoor.targetDoor = this;
+            else Gizmos.DrawLine(transform.position, targetDoor.transform.position);
+        }
+        //transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+
+    }
+
+    void OnEnable()
+    {
+        EventManager.StartListening(GameEvent.controlEnable, EnableControl);
+        EventManager.StartListening(GameEvent.controlDisable, DisableControl);
+        //currentScene = transform.parent.gameObject;
+    }
+    // Use this for initialization
+    void Start()
+    {
+        if (targetDoor) {
+            if (!targetDoor.targetDoor && !onWayDoor) targetDoor.targetDoor = this;
+            //else Gizmos.DrawLine(transform.position, targetDoor.transform.position);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this was used to control the animation being played
+        //if (Input.GetButtonDown("Interact") && playerNearby && targetDoor && controlEnabled) {
+        //if (woodDoor) {
+        //    TransitionManager.StartTransition(TransitionManager.DoorType.WoodDoor, 0.75f);
+        //} else if (stairs) {
+        //    TransitionManager.StartTransition(TransitionManager.DoorType.Stair, 0.75f);
+        //} else {
+        //    TransitionManager.StartTransition(TransitionManager.DoorType.MetalDoor, 0.75f);
+        //}
+        //    StartCoroutine(Wait(0.1f, DoorInteract));
+        //}
+    }
+
+    void DisableControl()
+    {
+        controlEnabled = false;
+        //Debug.Log("Control Disabled for the door");
+    }
+
+    void EnableControl()
+    {
+        controlEnabled = true;
+        //Debug.Log("Control Enabled for the door");
+    }
+
+    IEnumerator Wait(float time, UnityAction method)
+    {
+        yield return new WaitForSeconds(time);
+        method.Invoke();
+    }
+
+    //when the player push the interact button, this got called
+    //private void DoorInteract()
+    //{
+    //    //the player have pushed the interact button near the door
+    //    targetDoor.DoorOpen(player);
+    //    PlayerExited();
+    //    transform.parent.gameObject.SetActive(false);
+    //    targetDoor.transform.parent.gameObject.SetActive(true);
+    //}
+
+    public void DoorOpen(GameObject player)
+    {
+        //make a sound, pause 0.5s, move the player position, etc.
+        //Debug.Log("teleported");
+        PlayerEntered();
+        player.transform.position = transform.position;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) {
+            player = other.gameObject;
+            PlayerEntered();
+        }
+    }
+
+    void PlayerEntered()
+    {
+        playerNearby = true;
+        //todo: activate the hint
+        //Debug.Log("player entered");
+        //EventManager.StartListening(GameEvent.interact, DoorInteract);
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && playerNearby) {
+            playerNearby = false;
+            //todo: deactivate the hint
+            //Debug.Log("player exited");
+        }
+    }
+
+    void PlayerExited()
+    {
+        playerNearby = false;
+        //todo: deactivate the hint
+        //Debug.Log("player exited");
+        //EventManager.StopListening(GameEvent.interact, DoorInteract);
+    }
+
+    void OnDisable()
+    {
+        EventManager.StopListening(GameEvent.controlDisable, DisableControl);
+        EventManager.StopListening(GameEvent.controlEnable, EnableControl);
+        //EventManager.StopListening(GameEvent.interact, DoorInteract);
+    }
+}
---
+++ Pet/Assets/C#/EventManager.cs
@@ -0,0 +1,214 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+//use this script as the center of communication between scripts;
+//event manager for passing one float/byte/string parameter
+using System.Collections.ObjectModel;
+using System.Linq;
+public class EventManager : MonoBehaviour
+{
+    private Dictionary<GameEvent, UnityEvent> eventDictionary;
+    private Dictionary<string, UnityEvent> customEventDictionary;
+    //private Dictionary<EditorEvent, UnityEvent> editorEventDictionary;
+    private Dictionary<ParameterizedGameEvent, ObjEvent> objEventDictionary; //just in case if I need to pass some parameters to the event
+
+    private static EventManager eventManager;
+
+    public List<string> GetCustomEvents()
+    {
+
+        return instance.customEventDictionary.Keys.ToList();
+    }
+
+    public static EventManager instance
+    {
+        get
+        {
+            if (!eventManager) {
+                eventManager = FindObjectOfType(typeof(EventManager)) as EventManager;
+
+                if (!eventManager) {
+                    Debug.LogError("no eventmanager");
+                } else {
+                    eventManager.Init();
+                }
+            }
+
+            return eventManager;
+        }
+    }
+
+    void Init()
+    {
+        if (eventDictionary == null) {
+            eventDictionary = new Dictionary<GameEvent, UnityEvent>();
+        }
+
+        if (customEventDictionary == null) {
+            customEventDictionary = new Dictionary<string, UnityEvent>();
+        }
+
+        //if (editorEventDictionary == null) {
+        //	editorEventDictionary = new Dictionary<EditorEvent, UnityEvent>();
+        //}
+
+        if (objEventDictionary == null) {
+            objEventDictionary = new Dictionary<ParameterizedGameEvent, ObjEvent>();
+        }
+    }
+
+    public static void StartListening(ParameterizedGameEvent eventName, UnityAction<object> listener)
+    {
+        ObjEvent thisEvent = null;
+        if (instance.objEventDictionary.TryGetValue(eventName, out thisEvent)) {
+            thisEvent.AddListener(listener);
+        } else {
+            thisEvent = new ObjEvent();
+            thisEvent.AddListener(listener);
+            instance.objEventDictionary.Add(eventName, thisEvent);
+        }
+    }
+
+    //no parameter
+    public static void StartListening(GameEvent eventName, UnityAction listener)
+    {
+        UnityEvent thisEvent = null;
+        if (instance.eventDictionary.TryGetValue(eventName, out thisEvent)) {
+            thisEvent.AddListener(listener);
+        } else {
+            thisEvent = new UnityEvent();
+            thisEvent.AddListener(listener);
+            instance.eventDictionary.Add(eventName, thisEvent);
+        }
+    }
+
+    public static void StartListening(string eventName, UnityAction listener)
+    {
+        UnityEvent thisEvent = null;
+        if (instance.customEventDictionary.TryGetValue(eventName, out thisEvent)) {
+            thisEvent.AddListener(listener);
+        } else {
+            thisEvent = new UnityEvent();
+            thisEvent.AddListener(listener);
+            instance.customEventDictionary.Add(eventName, thisEvent);
+        }
+    }
+
+    //public static void StartListening(EditorEvent eventName, UnityAction listener)
+    //{
+    //	UnityEvent thisEvent = null;
+    //	if (instance.editorEventDictionary.TryGetValue(eventName, out thisEvent)) {
+    //		thisEvent.AddListener(listener);
+    //	} else {
+    //		thisEvent = new UnityEvent();
+    //		thisEvent.AddListener(listener);
+    //		instance.editorEventDictionary.Add(eventName, thisEvent);
+    //	}
+
+    //}
+
+    public static void StopListening(ParameterizedGameEvent eventName, UnityAction<object> listener)
+    {
+        if (eventManager == null) return;
+        ObjEvent thisEvent = null;
+        if (instance.objEventDictionary.TryGetValue(eventName, out thisEvent)) {
+            thisEvent.RemoveListener(listener);
+        }
+    }
+
+    public static void StopListening(GameEvent eventName, UnityAction listener)
+    {
+        if (eventManager == null) return;
+        UnityEvent thisEvent = null;
+        if (instance.eventDictionary.TryGetValue(eventName, out thisEvent)) {
+            thisEvent.RemoveListener(listener);
+        }
+    }
+
+    public static void StopListening(string eventName, UnityAction listener)
+    {
+        if (eventManager == null) return;
+        UnityEvent thisEvent = null;
+        if (instance.customEventDictionary.TryGetValue(eventName, out thisEvent)) {
+            thisEvent.RemoveListener(listener);
+        }
+    }
+
+    //public static void StopListening(EditorEvent eventName, UnityAction listener)
+    //{
+    //	if (eventManager == null) return;
+    //	UnityEvent thisEvent = null;
+    //	if (instance.editorEventDictionary.TryGetValue(eventName, out thisEvent)) {
+    //		thisEvent.RemoveListener(listener);
+    //	}
+
+    //}
+
+    public static void TriggerEvent(ParameterizedGameEvent eventName, object objParameter)
+    {
+        ObjEvent thisEvent = null;
+        if (instance.objEventDictionary.TryGetValue(eventName, out thisEvent)) {
+            thisEvent.Invoke(objParameter);
+        }
+    }
+
+    public static void TriggerEvent(GameEvent eventName)
+    {
+        UnityEvent thisEvent = null;
+        if (instance.eventDictionary.TryGetValue(eventName, out thisEvent)) {
+            thisEvent.Invoke();
+        }
+    }
+
+    public static void TriggerEvent(string eventName)
+    {
+        UnityEvent thisEvent = null;
+        if (instance.customEventDictionary.TryGetValue(eventName, out thisEvent)) {
+            thisEvent.Invoke();
+        }
+    }
+
+
+    //public static void TriggerEvent(EditorEvent eventName)
+    //{
+    //	UnityEvent thisEvent = null;
+    //	if (instance.editorEventDictionary.TryGetValue(eventName, out thisEvent)) {
+    //		thisEvent.Invoke();
+    //	}
+
+    //}
+}
+
+public class ObjEvent : UnityEvent<object> { }
+
+public enum GameEvent
+{
+    interact,
+    controlEnable,
+    controlDisable,
+    abilityOn,
+    abilityOff,
+    loadArea, //this event must be called AFTER the player is teleported
+    fadeOut, //fade out the scene, fade in the black screen
+    fadeIn, //fade out the black screen so the player can see the scene
+    switchTimeline,
+    updatePhysics,
+    openInventory //open up the inventory interface
+}
+
+public enum ParameterizedGameEvent
+{
+    select, //byte
+    switchControl,//Gameobject
+    doorOpen, //door type object
+    damageReceive, //int
+    getObject //(String) object name, tell the inventory manager that we got something
+}
+
+//these are all the events happening in the editor mode
+public enum EditorEvent
+{
+    gridChanged,
+}

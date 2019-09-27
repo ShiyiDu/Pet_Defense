c4123c9ab3447eacba8aa56a2c7a9c1932589b6b
Kristen <kristen_grinyer@icloud.com>
2019-09-27 12:42:27 -0400

    title scene added

---
+++ Assets/C#/Scene Changes/sceneChanges.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneChanges : MonoBehaviour
+{
+
+    public int sceneNumber;
+
+    public void toScene()
+    {
+        SceneManager.LoadScene(sceneNumber);
+    }
+}

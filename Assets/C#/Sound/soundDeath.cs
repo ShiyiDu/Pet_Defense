993799b898be607741f95ab661a3d353ea942449
Kristen <kristen_grinyer@icloud.com>
2019-10-22 22:38:25 -0400

    added sound effects

---
+++ Assets/C#/Sound/soundDeath.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundDeath : MonoBehaviour
+{
+    public AudioClip ghostDeath;
+    public AudioClip petDeath;
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+
+
+    //ghost dies
+    
+     //   audioSource.PlayOneShot(ghostDeath);
+    
+
+    //pet dies
+    
+      //  audioSource.PlayOneShot(petDeath);
+    
+}

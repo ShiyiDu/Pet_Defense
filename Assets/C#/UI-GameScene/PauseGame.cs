a8d8259d373b7170d8ad59912862d14b7030f2c4
Kristen <kristen_grinyer@icloud.com>
2019-10-26 22:36:58 -0400

    Updated UI (script changed)

--- Assets/C#/UI-GameScene/PauseGame.cs
+++ Assets/C#/UI-GameScene/PauseGame.cs
@@ -7,21 +7,27 @@ public class PauseGame : MonoBehaviour
 {
 
     public GameObject FadePanel;
-    public GameObject pause;
-    public Toggle btn;
-    public GameObject playText;
-    public Text btnText;
+    public GameObject pauseCentreText;
+    public Toggle toggle;
     public Button menuB;
+
+    public Sprite pauseImage;
+    public Sprite playImage;
+    public Image background;
+    
 
     private bool btnLastState = false; //the last state of the toggle, so we don't repeatly continue the game
 
+
     public void Update()
     {
-        if (btn.isOn == true) {
-            pause.SetActive(false);
-            playText.SetActive(false);
-            btnText.text = "Pause";
+        if (toggle.isOn == true) {
+
+            background.sprite = pauseImage;
+            
+            pauseCentreText.SetActive(false);
             FadePanel.SetActive(false);
+
             //resume all game content (ghosts, pets) (same as a method in the MenuPopup script)
             if (!btnLastState) PetUtility.ContinueGame();
             btnLastState = true;
@@ -29,12 +35,13 @@ public class PauseGame : MonoBehaviour
         }
 
 
-        if (btn.isOn == false) {
+        if (toggle.isOn == false) {
+
+            background.sprite = playImage;
 
             FadePanel.SetActive(true);
-            pause.SetActive(true);
-            playText.SetActive(true);
-            btnText.text = "Play";
+            pauseCentreText.SetActive(true);
+ 
             //pause all game content (ghosts, pets) (same as a method in the MenuPopup script)
             PetUtility.PauseGame();
             if (btnLastState) PetUtility.PauseGame();

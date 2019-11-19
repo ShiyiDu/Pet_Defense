a6ae6389cb4f7985d32fffeda30a4bee30cd24a0
Leo <shiyi.du@qq.com>
2019-11-18 23:27:04 -0500

    Merge
    redundent files deleted, old scene moved to Archieve folder

---
+++ Assets/C#/Interact/SpeechBubble.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeechBubble : MonoBehaviour
+{
+    public GameObject following;
+    public float offset = 0.7f;
+    // Start is called before the first frame update
+    private void OnEnable()
+    {
+        transform.localScale = Vector3.zero;
+        PetUtility.Coroutine(PetUtility.LinearZoom(Vector3.zero, Vector3.one, 0.1f, transform));
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!following) return;
+        Vector2 desired = following.transform.position;
+        desired.y += offset * following.transform.localScale.y;
+        transform.position = desired;
+    }
+}
